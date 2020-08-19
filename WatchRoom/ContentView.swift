02383b5797d6366f7ac67929a7c762e9c3e185b2
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-08-19 14:57:04 +1000

    Initial Commit

---
+++ WatchRoom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchRoom
+//
+//  Created by Bogdan on 19/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchRoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchRoom
+//
+//  Created by Bogdan on 19/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
